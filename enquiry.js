0a180d85e2d744b095086f9ba8a5bf0ee3a6d0cc
parthsutar9311 <parthsutar5055@gmail.com>
2026-03-09 15:23:09 +0530

    Create enquiry.js

---
+++ enquiry.js
@@ -0,0 +1,23 @@
+function validateForm()
+{
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+
+if(name == "")
+{
+alert("Please enter your name");
+return false;
+}
+
+if(email == "")
+{
+alert("Please enter your email");
+return false;
+}
+
+alert("Thank you for your enquiry!");
+
+return true;
+
+}
